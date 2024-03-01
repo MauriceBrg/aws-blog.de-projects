82b8d3360879cab4ea48cee7ae243b27a38ed9b9
Maurice Borgmeier <mborgmeier@tecracer.de>
2024-03-01 15:09:12 +0100

    Initial commit for serverless dash

---
+++ sam-dash/frontend/app.py
@@ -0,0 +1,63 @@
+import json
+
+from functools import lru_cache
+
+from apig_wsgi import make_lambda_handler
+
+from dash_app import build_app
+
+
+@lru_cache(maxsize=5)
+def build_handler(url_prefix: str) -> "Dash":
+
+    # If there's not prefix, it's a custom domain
+    if url_prefix is None or url_prefix == "":
+        return make_lambda_handler(wsgi_app=build_app().server, binary_support=True)
+
+    # If there's a prefix we're dealing with an API gateway stage
+    # and need to return the appropriate urls.
+    return make_lambda_handler(
+        wsgi_app=build_app({"url_base_pathname": url_prefix}).server,
+        binary_support=True,
+    )
+
+
+def get_raw_path(apigw_event: dict) -> str:
+    """
+    The "raw" path that was requested (i.e. including the stage prefix) is hidden
+    under the requestContext object.
+    """
+
+    return apigw_event.get("requestContext", {}).get("path", apigw_event["path"])
+
+
+def get_url_prefix(apigw_event: dict) -> str:
+    """
+    Returns the stage url prefix if the request arrives from the API Gateway and
+    an empty string if it arrives via a custom domain.
+    """
+
+    apigw_stage_name = apigw_event["requestContext"]["stage"]
+    prefix = f"/{apigw_stage_name}/"
+    raw_path = get_raw_path(apigw_event)
+
+    if raw_path.startswith(prefix):
+        return prefix
+
+    return ""
+
+
+def lambda_handler(
+    event: dict[str, "Any"], context: dict[str, "Any"]
+) -> dict[str, "Any"]:
+
+    print(json.dumps(event))
+
+    # We need the path with the stage prefix, which the API gateway hides a bit.
+    event["path"] = get_raw_path(event)
+    handle_event = build_handler(get_url_prefix(event))
+
+    print(json.dumps(event))
+    response = handle_event(event, context)
+    # print(json.dumps(response, indent=2, sort_keys=True))
+    return response
---
+++ sam-dash/tests/integration/test_api_gateway.py
@@ -0,0 +1,45 @@
+import boto3
+import pytest
+import requests
+
+
+STACK_NAME = "sam-dash"
+APIGW_URL_OUTPUT_NAME = "ServerlessDashApi"
+
+
+class TestApiGateway:
+
+    @pytest.fixture()
+    def api_gateway_url(self):
+        """Get the API Gateway URL from Cloudformation Stack outputs"""
+
+        client = boto3.client("cloudformation")
+
+        try:
+            response = client.describe_stacks(StackName=STACK_NAME)
+        except Exception as e:
+            raise RuntimeError(
+                f"Cannot find stack {STACK_NAME} \n"
+                f'Please make sure a stack with the name "{STACK_NAME}" exists'
+            ) from e
+
+        stacks = response["Stacks"]
+        stack_outputs = stacks[0]["Outputs"]
+        api_outputs = [
+            output
+            for output in stack_outputs
+            if output["OutputKey"] == APIGW_URL_OUTPUT_NAME
+        ]
+
+        if not api_outputs:
+            raise KeyError(f"{APIGW_URL_OUTPUT_NAME} not found in stack {STACK_NAME}")
+
+        return api_outputs[0]["OutputValue"]  # Extract url from stack outputs
+
+    def test_api_gateway(self, api_gateway_url):
+        """Call the API Gateway endpoint and check the response"""
+        response = requests.get(api_gateway_url, timeout=10)
+
+        assert response.status_code == 200
+        loading_text = "Loading..."
+        assert loading_text in response.text
